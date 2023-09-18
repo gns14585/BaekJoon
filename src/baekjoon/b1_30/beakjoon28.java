6454a5fd77307b02a1d48d7d1ab63135354622dc
LeeJungHoon <gns14585@naver.com>
2023-09-18 12:33:00 +0900

    baekjoon

---
+++ src/baekjoon/b1_30/beakjoon28.java
@@ -0,0 +1,13 @@
+package baekjoon.b1_30;
+
+import java.util.Scanner;
+
+public class beakjoon28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+    }
+}
